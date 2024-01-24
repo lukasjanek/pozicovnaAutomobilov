3434360e72b801f367f33c7b41ad012568cc1f14
lukasjanek <55839833+lukasjanek@users.noreply.github.com>
2024-01-24 13:46:19 +0100

    Update ReserveView.vue

--- src/views/ReserveView.vue
+++ src/views/ReserveView.vue
@@ -1,15 +1,15 @@
 <template>
-  <div style="margin: 20px; width: 1800px; height: 800px;">
+  <div style="margin: 20px; width: 1600px; height: 800px;">
     <div class="mt-5  smallcontainer" style="float: left; width: 670px;">
         <img :src="'/src/assets/images/' + selectedCar.image" class="carPic" alt="Car" />
     </div>
-  <div class="mt-5  smallercontainer" style="float: left; width: 530px;">
+  <div class="mt-5  smallercontainer" style="float: left; width: 450px;">
             <h6>{{ selectedCar.title }}</h6>
             <h6>{{ selectedCar.price }}</h6>
             <p> {{ selectedCar.text }}</p>
             <p> {{ selectedCar.desc }}</p>
     </div>
-  <div class="mt-5 smallercontainer" style="float: left; width: 550px;">
+  <div class="mt-5 smallercontainer" style="float: left; width: 450px;">
     <Calendar @date-selected="handleDateSelected"/>
     <button @click="reserveCar" class="btn btn-primary">Rezervuj</button>
   </div>
